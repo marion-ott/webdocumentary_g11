9af765421116a4c6365c3492c475fcb744ea882e
Manon Ragnotti <m.ragnotti@mazarinedigital.com>
2019-01-22 17:17:01 +0100

    Try checked input system

--- client/src/components/FormIntro/Form/Form.js
+++ client/src/components/FormIntro/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserInfo from './steps/UserInfo'
+import ButtonNext from './steps/ButtonNext'
 import content from '../../../content/content'
 
 class Form extends React.Component {
@@ -9,23 +10,31 @@ class Form extends React.Component {
             firstName: '',
             age: '',
             gender: '',
+            nextStep: false
         }
     }
 
     nextStep = () => {
-        const step = this.state.step + 1
-        this.setState({
-            step
-        })
+        const inputs = document.getElementsByTagName('input');
+        for ( let i = 0; i < inputs.length; i++) {
+           if(inputs[i].value !== '') return true;
+           this.setState({
+               nextStep: true
+           })
+        }
+        alert('Veuillez remplir tous les champs !');
+        return false;
     }
 
     render() {
-        return <UserInfo
-                    nextStep={this.nextStep}
-                    //handleChange = {this.handleChange}
-                    //values={values}
-                />
-
+        return (
+            <UserInfo
+                nextStep={this.nextStep}
+                //handleChange = {this.handleChange}
+                //values={values}
+                
+            />
+        )
     }
 }
 
