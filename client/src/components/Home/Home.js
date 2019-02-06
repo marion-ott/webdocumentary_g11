dd16efead9ea9d629f2f74104e43c37c90dba3d8
ME <me@MacBook-Pro-de-ME.local>
2019-02-06 14:19:23 +0100

    adding annimation

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,37 +1,229 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Cta from '../../blocs/Cta/Cta'
+//import './home2.css'
 import css from './home.scss'
 
-class Home extends React.Component {
-    render() {
-        //const data = content.data[this.props.step].home
-        return(
-            <section className={css.component}>
-                <div className="center">
-                    <div className="bubbles">
-                        <div className="filledBubble"></div>
-                        <div className="filledBubble"></div>
-                        <div className="filledBubble"></div>
-                        <div className="filledBubble"></div>
-                        <div className="borderBubble"></div>
-                        <div className="borderBubble"></div>
-                        <div className="borderBubble"></div>
-                        <div className="borderBubble"></div>
-                        <div className="borderBubble"></div>
-                    </div>
-                    <div className="titleBubble">
-                        <h1>
-                            <span className="dans">dans</span>
-                            <span className="la">la</span>
-                            <span className="bulle">bulle</span>
-                        </h1>
-                    </div>
-                </div>
-                <Cta text="Démarrer l'expérience" scrollSection={this.props.scrollSection}/>
-                {/* <ScrollSymbol /> */}
-            </section>
-        )
+class Canvas extends Component {
+    componentWillMount() {
+        this.setState({
+            canvasSize: { canvasWidth: 8, canvasHeight: 600 }
+        })
     }
-}
 
-export default Home
+    componentDidMount() {
+        const canvas = this.canvas
+        //const canvas2 = this.canvas2
+
+        const c = canvas.getContext('2d');
+        // const cc = canvas2.getContext('2d');
+
+        // cc.font = "bold 20px verdana, arial, serif";
+        // cc.fillStyle= 'black';
+        // cc.fillText('Dans', 0, 50,);
+        // cc.fillText('la', 20, 100);
+        // cc.fillText('Bulle', 0, 130);
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const mouse = {
+            x: 10,
+            y: 10
+        };
+
+        const colors = [
+        '#2185C5',
+        '#7ECEFD',
+        '#FFF6E5',
+        '#FF7F66'
+        ];
+
+        window.addEventListener('mousemove', function (event) {
+            mouse.x = event.clientX;
+            mouse.y = event.clientY;
+        });
+
+        window.addEventListener('resize', function() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            init();
+        });
+
+
+        function getDistance(x1, y1, x2, y2) {
+        let xDistance = x2 - x1;
+        let yDistance = y2 - y1;
+
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2)
+        )};
+        function randomIntFromRange(min,max) {
+            return Math.floor(Math.random() * (max - min -1) + min);
+        }
+
+        function randomColor(colors) {
+            return colors[Math.floor(Math.random() * colors.length)];
+        }
+
+        // Event Listeners
+
+
+        function distance(x1, y1, x2, y2) {
+            const xDist = x2 - x1;
+            const yDist = y2 - y1;
+
+            return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+        }
+
+        function rotate(velocity, angle) {
+        const rotateVelocities = {
+            x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+            y: velocity.x * Math.sin(angle) - velocity.y * Math.cos(angle)
+        };
+        return rotateVelocities;
+        }
+
+
+        function resolveCollision(particle, otherParticle) {
+        const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+        const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+        const xDist = otherParticle.x - particle.x;
+        const yDist = otherParticle.y - particle.y;
+
+        if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+            const angle = Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+            const m1 = particle.mass;
+            const m2 = otherParticle.mass;
+
+            const u1 = rotate(particle.velocity, angle);
+            const u2 = rotate(otherParticle.velocity, angle);
+
+            const v1 = {x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 /(m1 + m2), y: u1.y};
+            const v2 = {x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * -1 * m2 /(m1 + m2), y: u2.y};
+
+            const vFinal1 = rotate(v1, -angle);
+            const vFinal2 = rotate(v2, -angle);
+
+            particle.velocity.x = vFinal1.x;
+            particle.velocity.y = vFinal1.y;
+
+            otherParticle.velocity.x = vFinal2.x;
+            otherParticle.velocity.y = vFinal2.y;
+        }
+        }
+        // Objects
+        function Particle(x, y, radius, color) {
+            this.x = x;
+            this.y = y;
+            this.velocity = {
+            x: (Math.random() - 0.5) * 4,
+            y: (Math.random() - 0.5) * 4
+            }
+            this.radius = radius;
+            this.color = color;
+            this.mass = 1;
+            this.opacity = 0;
+
+            this.update = particles => {
+                this.draw();
+                for (var i = 0; i < particles.length; i++) {
+                if (this === particles[i]) continue;
+                if (distance(this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+                    resolveCollision(this, particles[i]);
+                }
+                }
+                if (this.x - this.radius <= 0  || this.x + this.radius >= window.innerWidth  ) {
+                this.velocity.x = -this.velocity.x;
+                }
+                if (this.y - this.radius <= 0  || this.y + this.radius >= window.innerHeight  ) {
+                this.velocity.y = -this.velocity.y;
+                }
+                if (distance(mouse.x, mouse.y, this.x, this.y) < 30) {
+                this.opacity += 1;
+                }else if (this.opacity > 0) {
+                this.opacity -= 1;
+                this.opacity = Math.max(0, this.opacity)
+                }
+                this.x += this.velocity.x;
+                this.y += this.velocity.y;
+            };
+
+            this.draw = ()  => {
+                c.beginPath();
+                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                c.save();
+                c.globalAlpha = this.opacity;
+                c.fillStyle = this.color;
+                c.fill();
+                c.restore();
+                c.strokeStyle = this.color;
+                c.stroke();
+                c.closePath();
+            };
+        }
+
+
+        // Implementation
+        let particles;
+        // particles = [];
+        function init() {
+            particles = [];
+
+            for (let i = 0; i < 20; i++) {
+            const radius = Math.floor(Math.random() * 40);
+            let x = randomIntFromRange(radius, canvas.width - radius);
+            let y = randomIntFromRange(radius, canvas.height - radius);
+            //let y = Math.random () * innerHeight;
+            const color = '#0000FF';
+
+            if (i !== 0) {
+                for (let j = 0; j < particles.length; j++) {
+                if (distance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+                    x = randomIntFromRange(radius, canvas.width - radius);
+                    y = randomIntFromRange(radius, canvas.height - radius);
+                    j = -1;
+                }
+                }
+            }
+
+
+            particles.push(new Particle(x, y, radius, color));
+            console.log(particles);
+            console.log(x);
+
+            };
+            //console.log(particles);
+        }
+        console.log(Math.random () * window.innerHeight);
+        // Animation Loop
+        function animate() {
+            requestAnimationFrame(animate);
+            c.clearRect(0, 0, canvas.width, canvas.height);
+
+            //c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+            particles.forEach(particle => {
+            particle.update(particles);
+            });
+        }
+        init();
+        animate();
+
+            }
+            render(){
+                return(
+                    <section className={css.component}>
+                        <div className="divGeneral">
+                          {/* <div className="divTwo">
+                             <canvas className="canvaTwo" ref={el => this.canvas2 = el}></canvas>
+                          </div> */}
+                        <canvas className="canvaOne" ref={el => this.canvas = el}></canvas>
+                        </div>
+                        <Cta text="Démarrer l'expérience" scrollSection={this.props.scrollSection}/>
+                    </section>
+                    
+                );
+            }
+        }
+        export default Canvas;
