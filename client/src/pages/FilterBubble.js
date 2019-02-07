d3278086e808d6a3474885e646506295d350c69a
Manon Ragnotti <manon.ragnotti@hotmail.fr>
2019-02-07 23:36:00 +0100

    Style

--- client/src/pages/FilterBubble.js
+++ client/src/pages/FilterBubble.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-//import Tube from '../components/OurBubble/OurBubble'
+import Tube from '../components/OurBubble/OurBubble'
 import content from '../content/content'
 import BubbleCanvas from '../components/BubbleCanvas/BubbleCanvas'
 import Text from '../components/Text/Text'
@@ -36,13 +36,11 @@ class FilterBubble extends React.Component {
                     color="black"
                     txtColor={data.FilterBubble[0].txtColor}
                     hasBlocks={true}
-                    paragraph={data.FilterBubble[2].text}
-                    //backgroundColor={data.FilterBubble[1].backgroundColor}
+                    paragraph={data.FilterBubble[1].text}
                     blocks={data.FilterBubble[2].blocks}
                 />
                 <Text
-                    paragraph={data.FilterBubble[3].text}
-                    //backgroundColor={data.FilterBubble[0].backgroundColor}
+                    paragraph={data.FilterBubble[2].text}
                     txtColor={data.FilterBubble[0].txtColor}
                     className={data.FilterBubble[0].className}
                 />
