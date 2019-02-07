5451f31a54def8219a2d4071262c04f11d407670
Manon Ragnotti <manon.ragnotti@hotmail.fr>
2019-02-07 22:37:30 +0100

    cta typo

--- client/src/components/AlgoPage/AlgoIntro.js
+++ client/src/components/AlgoPage/AlgoIntro.js
@@ -2,7 +2,6 @@ import React from 'react'
 import css from './algoPage.scss'
 import content from '../../content/content'
 import ScrollSymbol from '../../blocs/ScrollSymbol/ScrollSymbol'
-import AlgoBackground from './AlgoBackground'
 import videoSrc from './algo_bg_video.mp4'
 
 
